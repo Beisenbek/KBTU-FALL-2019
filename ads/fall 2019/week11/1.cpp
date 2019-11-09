f6c9c375fc5a93313732653fa6a883eaca4f5056
Бейсенбек Байсаков <bsnbk@outlook.com>
2019-11-09 11:00:55 +0600

    'upd'

---
+++ ads/fall 2019/week11/1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct node{
+    map<char, node *> next; 
+    bool endOfWord;
+    string prefix;
+    node(string pr){
+        endOfWord = false;
+        prefix = pr;
+    }
+};
+
+struct trie{
+    node * root;
+    trie(){
+        root = new node("/");
+    }
+    void addString(string s){
+        node * cur = root;
+        for(int i = 0; i < s.size(); ++i){
+            if(cur->next.find(s[i]) == cur->next.end()){
+                cur->next[s[i]] = new node(cur->prefix + s[i]);
+            }
+            cur = cur->next[s[i]]; 
+        }
+        cur->endOfWord = true;
+    }
+
+    bool find(string s){
+        node * cur = root;
+        bool found = true;
+        for(int i = 0; i < s.size(); ++i){
+            if(cur->next.find(s[i]) == cur->next.end()){
+                found = false;
+                break;
+            }
+            cur = cur->next[s[i]]; 
+        }
+        return found && cur->endOfWord;
+    }
+
+    void printNode(node * cur){
+        cout << cur->prefix << " is it endOfWord: " << cur->endOfWord << "; childs: ";
+        map<char,node *>:: iterator it;
+        for(it = cur->next.begin(); it != cur->next.end(); ++it){
+            cout << it->first << " ";
+        }
+        cout << endl;
+    }
+
+   
+
+    void print(node * cur){
+        printNode(cur); 
+        map<char, node *>:: iterator it;
+        for(it = cur->next.begin(); it != cur->next.end(); ++it){
+            print(it->second);
+        }     
+    }
+};
+
+int main(){
+
+    string arr[] = {"hello", "world","hi", "hel"};
+    string test[] = {"hello", "worl", "hi", "h"};
+
+    trie t;
+    
+    for(int i = 0; i < 4; ++i){
+        t.addString(arr[i]);
+    }
+
+    //t.print(t.root);
+    
+    for(int i = 0; i < 4; ++i){
+        cout << t.find(test[i]) << " ";
+    }
+
+    return 0;
+}
