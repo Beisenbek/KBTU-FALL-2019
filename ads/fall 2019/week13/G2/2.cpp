4d2d26729df3580e1fe2d7db94635583798a12c9
Beisenbek Baisakov <beysenbek@gmail.com>
2019-11-26 14:30:12 +0600

    upd

--- ads/fall 2019/week13/G2/2.cpp
+++ ads/fall 2019/week13/G2/2.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 #include <set>
-#include <vector>
-#define ull long 
-#define Q 18446744073709551615 
+#define ull unsigned long 
+#define Q 100007
+#define MS 1000001           
 
 using namespace std;
 
-vector<ull> p_i, h_i;
+ull p_i[MS], h_i[MS];
 set<ull> ds;
 
 void prepareP(string text){
     ull x;
-    ull n = text.size() + 100;
-    p_i.push_back(1);
+    ull n = text.size();
+    p_i[0] = 1;
     for(ull i = 1; i < n; i++){
         ull l = (p_i[i-1] * 31) % Q;
-        p_i.push_back(l);
+        p_i[i] = l;
     }
 }
 
 void calculateHashForAllPrefix(string text){
     ull n = text.size();
-    h_i.push_back(text[0] - 'a' + 1);
+    h_i[0] = (text[0] - 'a' + 1);
     for(ull i = 1; i < n; ++i){
         ull k = (text[i] - 'a' + 1);
-        h_i.push_back((k * p_i[i]) % Q);
+        h_i[i] = k * p_i[i] % Q;
         if(i > 0){
-            h_i[i] += h_i[i-1];
+            h_i[i] = (h_i[i] + h_i[i-1])% Q;
         }
     }
 }
 
-ull calculateHash(string pattern, int ttt){
+ull calculateHash(string pattern){
     ull h_p = 0;
     ull n = pattern.size();
 
-    if(ttt == 46) {
-        cout << "test" << endl;
-        return 1;
-    }
-
     for(ull i = 0; i < n; ++i){
         ull k = (pattern[i] - 'a' + 1);
-        h_p = (h_p +  (k * p_i[i]) % Q) % Q;
+        h_p = (h_p +  k * p_i[i] % Q) % Q;
     }
+
     return h_p;
 }
 
@@ -51,43 +47,35 @@ int main(){
     string s;
     cin >> s;
 
-     ull t;
+    ull t;
     cin >> t;
 
     prepareP(s);
 
     calculateHashForAllPrefix(s);
 
-    
-
-   
     string str;
     bool ok = false;
 
-
     for(ull i = 0; i < t; ++i){
         cin >> str;
-        ull x = calculateHash(str, t);
+        ull x = calculateHash(str);
         ds.insert(x);
     }
-
   
-
     ull k = s.size();
     for(ull i = 0; i < k - 1; ++i){
-        ull sh = (h_i[k - 1] - h_i[i]) / p_i[i + 1];
-        ull ph = h_i[i];
-        //cout << ph << " " << sh << endl;
-        //cout << s.substr(0, i + 1) << " " << s.substr(i + 1) << endl;
-        //cout << calculateHash(s.substr(0, i + 1)) << " " << calculateHash(s.substr(i + 1)) << endl;
-        //cout << ph << " " << sh << endl;
-        if(ds.find(ph) != ds.end() && ds.find(sh) != ds.end()){
-            ok = true;
-            break;
+        if(ds.find(h_i[i]) != ds.end()){
+            string q = s.substr(i + 1);
+            ull qh = calculateHash(q);
+            if(ds.find(qh) != ds.end()){
+                ok = true;
+                break;
+            }
         }
     }
 
-     if(ok){
+    if(ok){
         cout << "YES";
     }else{
         cout << "NO";
